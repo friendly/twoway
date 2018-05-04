a4eaf4574edf838d3eccc30a21dafe1782974198
Michael Friendly <friendly@yorku.ca>
2018-05-04 15:44:36 -0400

    work on twoway and split out fitting functions

---
+++ R/medianfit.R
@@ -0,0 +1,62 @@
+#' Fit a two-way table using median polish
+#'
+#' @param x a numeric matrix
+#' @param ... other arguments passed down
+#' @importFrom stats medpolish
+#' @export
+#' @result An object of class \code{c("twoway", "medpolish")} with the following named components:
+#' \describe{
+#'  \item{overall}{the fitted constant term.}
+#'  \item{row}{the fitted row effects.}
+#'  \item{col}{the fitted column effects.}
+#'  \item{residuals}{the residuals.}
+#'  \item{name}{the name of the dataset.}
+#' }
+
+
+medianfit <- function(x, trace.iter=FALSE, ...) {
+  result <- stats::medpolish(x, trace.iter=trace.iter, ...)
+  result$rownames <- rownames(x)
+  result$colnames <- colnames(x)
+  result$name <-  deparse(substitute(x))
+  #	result$roweff <- result$row - result$overall
+  #	result$coleff <- result$col - result$overall
+  result$method <- "median"
+  class(result) <- c("twoway", "medpolish")
+  result
+}
+
+#' Fit a two-way table using row and column means
+#'
+#' @param x a numeric matrix
+#' @param ... other arguments passed down
+#' @export
+#' @result An object of class \code{c("twoway")} with the following named components:
+#' \describe{
+#'  \item{overall}{the fitted constant term.}
+#'  \item{row}{the fitted row effects.}
+#'  \item{col}{the fitted column effects.}
+#'  \item{residuals}{the residuals.}
+#'  \item{name}{the name of the dataset.}
+#' }
+
+
+meanfit <- function(x, ...) {
+  z <- as.matrix(x)
+  nr <- nrow(z)
+  nc <- ncol(z)
+  r <- numeric(nr)
+  c <- numeric(nc)
+  overall <- mean(z)
+  row <- rowMeans(z) - overall
+  col <- colMeans(z) - overall
+  residuals <- z - outer(row, col) - overall
+  names(row) <- rownames(z)
+  names(col) <- colnames(z)
+  ans <- list(overall = overall, row = row, col = col, residuals = residuals,
+              name = deparse(substitute(x)),
+              method = "mean")
+  class(ans) <- "twoway"
+  ans
+}
+
--- R/twoway.R
+++ R/twoway.R
@@ -1,6 +1,28 @@
 # Functions for Tukey anslysis of two-way tables
+
+#' Analysis of a two-way table
+#'
+#' Fits an additive model using either row and column means or Tukey's median polish procedure
+#'
+#' @param x a numeric matrix
+#' @param ... other arguments passed down
+#' @rdname twoway
+#' @seealso code{\link[stats]{medpolish}}
+#' @references Tukey, J. W. (1977). \emph{Exploratory Data Analysis}, Reading MA: Addison-Wesley.
+#'             Friendly, M. (1991). \emph{SAS System for Statistical Graphics} Cary, NC: SAS Institute
+#' @export
+
 twoway <-
 	function(x, ...) UseMethod("twoway")
+
+
+#' @param x a numeric matrix
+#' @param method one of \code{"mean"} or \code{"median"}
+#' @param ... other arguments passed down
+#' @rdname twoway
+#' @method twoway default
+#' @export
+#' @result
 
 twoway.default <- function(x, method=c("mean", "median"), ...) {
 
@@ -8,52 +30,19 @@ twoway.default <- function(x, method=c("mean", "median"), ...) {
   if (method=="mean")
     result <- meanfit(x)
   else
-    result <- medianfit(x)
-  
+    result <- medianfit(x, ...)
   result
-  
+
 }
 
-
-medianfit <- function(x, trace.iter=FALSE, ...) {
-	result <- stats::medpolish(x, trace.iter=trace,.ter, ...)
-	result$rownames <- rownames(x)
-	result$colnames <- colnames(x)
-	result$name <-  deparse(substitute(x))
-#	result$roweff <- result$row - result$overall
-#	result$coleff <- result$col - result$overall
-	result$method <- "median"
-  class(result) <- c("twoway", "medpolish")
-	result
-	}
-
-meanfit <- function(x, ...) {
-	z <- as.matrix(x)
-  nr <- nrow(z)
-  nc <- ncol(z)
-  r <- numeric(nr)
-  c <- numeric(nc)
- 	overall <- mean(z)
- 	row <- rowMeans(z) - overall
- 	col <- colMeans(z) - overall
- 	residuals <- z - outer(row, col) - overall
-  names(row) <- rownames(z)
-  names(col) <- colnames(z)
-  ans <- list(overall = overall, row = row, col = col, residuals = residuals, 
-      name = deparse(substitute(x)), 
-      method = "mean")
-  class(ans) <- "twoway"
-  ans 	
-	}
-
 ## diagnostic plot for removable interaction
-plot.medpolish <- 
-function (x, main = "Tukey Additivity Plot", ...) 
+plot.medpolish <-
+function (x, main = "Tukey Additivity Plot", ...)
 {
 		comp <- outer(x$row, x$col)/x$overall
 		res <- x$residuals
-    plot(comp, x$residuals, main = main, 
-        xlab = "Diagnostic Comparison Values", ylab = "Residuals", 
+    plot(comp, x$residuals, main = main,
+        xlab = "Diagnostic Comparison Values", ylab = "Residuals",
         ...)
     abline(lm(res ~ comp))
     abline(h = 0, v = 0, lty = "dotted")
@@ -61,14 +50,14 @@ function (x, main = "Tukey Additivity Plot", ...)
 
 
 print.twoway <-
-function (x, digits = getOption("digits"), ...) 
+function (x, digits = getOption("digits"), ...)
 {
-		title <- if(x$method == "mean") 
+		title <- if(x$method == "mean")
 			"Mean decomposition "
 			else
 			"Median polish decomposition "
-			
-#    cat("\nMedian Polish Results (Dataset: \"", x$name, "\")\n", 
+
+#    cat("\nMedian Polish Results (Dataset: \"", x$name, "\")\n",
 #        sep = "")
 		cat("\n", title, "(Dataset: \"", x$name, "\")\n", sep="")
     cat("\nOverall: ", x$overall, "\n\nRow Effects:\n", sep = "")
@@ -85,7 +74,7 @@ function (x, digits = getOption("digits"), ...)
 
 plot.twoway <- function(x, type=c("fit", "diagnose"), rfactor=1.5, ...) {
 	type <- match.arg(type)
-	
+
 	row <- x$row
 	col <- x$col
 	r <- length(row)
@@ -93,17 +82,17 @@ plot.twoway <- function(x, type=c("fit", "diagnose"), rfactor=1.5, ...) {
 	all <- x$overal
 	clo <- min(col) + all
 	chi <- max(col) + all
-  from <- cbind(clo - row, clo + row)	
+  from <- cbind(clo - row, clo + row)
   to   <- cbind(chi - row, chi + row)
 
   rlo <- min(row)
   rhi <- max(row)
-  from <- rbind(from,  cbind(col + all - rhi, col + all + rhi))	
-  to   <- rbind(to,    cbind(col + all - rlo, col + all + rlo))	
+  from <- rbind(from,  cbind(col + all - rhi, col + all + rhi))
+  to   <- rbind(to,    cbind(col + all - rlo, col + all + rlo))
   colnames(from) <- c("x", "y")
   colnames(to)   <- c("x", "y")
-  
+
   labs <- c(names(row), names(col))
-  
+
   plot( rbind(from, to) , col=rep(c("red", "blue"), times= c(r, c)))
 }
