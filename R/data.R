709289a52c1da9c56f42cb3039685096955870f0
Michael Friendly <friendly@yorku.ca>
2018-05-05 18:30:42 -0400

    added taskRT data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Data on reaction times for various tasks and topics
+#'
+#' A demonstration 3 x 4 two-way table composed of reaction times for
+#' tasks varying in difficulty, with content on different topics.
+#'
+#' @name taskRT
+#' @docType data
+#' @keywords data
+NULL
+
