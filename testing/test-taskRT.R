901a5e4e267fe8457b4625c4c2dbecc06ad144f0
Michael Friendly <friendly@yorku.ca>
2018-05-06 18:32:33 -0400

    testing..

--- testing/test-taskRT.R
+++ testing/test-taskRT.R
@@ -4,3 +4,9 @@ res <- twoway(taskRT)
 plot(res)
 
 plot(res, type="diagnose")
+
+# converting wide to long
+
+library(reshape2)
+long <- melt(as.matrix(taskRT))
+colnames(long) <- c("row", "col", "response")
