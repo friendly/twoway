18ce09a1f2072d2493244251bbd16a70040cbc57
Michael Friendly <friendly@yorku.ca>
2018-05-09 14:36:39 -0400

    ammend testing/ example

--- testing/test-sentRT.R
+++ testing/test-sentRT.R
@@ -1,9 +1,11 @@
+# test how to do the ANOVA for the two-way table, including Tukey's test for non-additivity
+
 data(sentRT)
 sent.2way <- twoway(sentRT)
 sent.df <- as.data.frame(sent.2way)
 
-aov(data ~ row + col, data=sent.df)
-aov(data ~ row + col + nonadd, data=sent.df)
+anova(aov(data ~ row + col, data=sent.df))
+anova(aov(data ~ row + col + nonadd, data=sent.df))
 
 sent.mod1 <- lm(data ~ row + col, data=sent.df)
 sent.mod2 <- lm(data ~ row + col + nonadd, data=sent.df)
