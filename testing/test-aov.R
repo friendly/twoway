54b0054c013a4299a7afed272e63cf9a4b62c12c
Michael Friendly <friendly@yorku.ca>
2018-05-10 10:19:18 -0400

    added a simple testing case for anova.twoway

---
+++ testing/test-aov.R
@@ -0,0 +1,16 @@
+data(sentRT)
+sent.2way <- twoway(sentRT)
+sent.df <- as.data.frame(sent.2way)
+
+aov1 <- anova(mod1 <- aov(data ~ row + col, data=sent.df))
+aov2 <- anova(mod2 <- aov(data ~ row + col + nonadd, data=sent.df))
+aov3 <- anova(mod1, mod2)
+
+sent.mod1 <- lm(data ~ row + col, data=sent.df)
+sent.mod2 <- lm(data ~ row + col + nonadd, data=sent.df)
+
+anova(sent.mod1)
+anova(sent.mod2)
+# test non-add
+anova(sent.mod1, sent.mod2)
+
