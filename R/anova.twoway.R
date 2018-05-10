e00f4ae265f8fa09feefd81b7159a68452ba46be
Michael Friendly <friendly@yorku.ca>
2018-05-10 10:36:54 -0400

    complete initial anova.twoway, now exported

--- R/anova.twoway.R
+++ R/anova.twoway.R
@@ -4,9 +4,10 @@
 #'
 #' @details At present, this function simply gives the results of the ANOVAs for the additive model, the model including the 1 df
 #'          term for non-additivity, and an \code{anova()} comparison of the two.
-#' @param x a \code{class("twoway")} object
+#' @param object a \code{class("twoway")} object
 #' @param ... other arguments passed down, but not used here
 #' @author Michael Friendly
+#' @importFrom stats anova aov
 #' @export
 #' @examples
 #' data(sentRT)
@@ -14,26 +15,26 @@
 #' anova(sent.2way)
 
 
-anova.twoway <- function(x, ...) {
+anova.twoway <- function(object, ...) {
 
-  # r <- length(x$row)
-  # c <- length(x$col)
+  # r <- length(object$row)
+  # c <- length(object$col)
   #
-  # fit <- outer(x$row, x$col, "+") + x$overall
-  # dat <- fit + x$residuals
+  # fit <- outer(object$row, object$col, "+") + object$overall
+  # dat <- fit + object$residuals
   #
-  # sse <- sum(x$residuals^2)
+  # sse <- sum(object$residuals^2)
   # dfe <- ( r - 1 ) * ( c - 1 )
   #
-  # ssrow <- c * sum(x$row^2)
-  # sscol <- r * sum(x$col^2)
+  # ssrow <- c * sum(object$row^2)
+  # sscol <- r * sum(object$col^2)
 
-  z <- as.data.frame(x)
+  z <- as.data.frame(object)
   aov1 <- anova(mod1 <- aov(data ~ row + col, data=z))
   aov2 <- anova(mod2 <- aov(data ~ row + col + nonadd, data=z))
   aov3 <- anova(mod1, mod2)
 
-  info <- paste0('(Dataset: ', '"', x$name, '"; ', 'method: "', x$method, '")')
+  info <- paste0('(Dataset: ', '"', object$name, '"; ', 'method: "', object$method, '")')
   cat("Additive model", info, "\n")
   print(aov1)
 
