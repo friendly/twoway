9f39ebb2d3cfa07531784b1ac4eb65554e173812
Michael Friendly <friendly@yorku.ca>
2018-05-15 11:33:27 -0400

    update test-aov.R

--- testing/test-aov.R
+++ testing/test-aov.R
@@ -1,3 +1,5 @@
+# testing how to do a twoway.anova method
+
 data(sentRT)
 sent.2way <- twoway(sentRT)
 sent.df <- as.data.frame(sent.2way)
@@ -5,6 +7,8 @@ sent.df <- as.data.frame(sent.2way)
 aov1 <- anova(mod1 <- aov(data ~ row + col, data=sent.df))
 aov2 <- anova(mod2 <- aov(data ~ row + col + nonadd, data=sent.df))
 aov3 <- anova(mod1, mod2)
+
+
 
 sent.mod1 <- lm(data ~ row + col, data=sent.df)
 sent.mod2 <- lm(data ~ row + col + nonadd, data=sent.df)
@@ -14,3 +18,5 @@ anova(sent.mod2)
 # test non-add
 anova(sent.mod1, sent.mod2)
 
+str(aov1)
+rownames(aov1)
