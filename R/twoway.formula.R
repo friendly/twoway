1a89bb3d0ea2a7647c78e791e02d86a11c2377e9
Michael Friendly <friendly@yorku.ca>
2018-05-17 10:23:56 -0400

    document stub of twoway.formula to pass R cmd check

--- R/twoway.formula.R
+++ R/twoway.formula.R
@@ -1,3 +1,16 @@
+#' Initial sketch for a twoway formula method
+#'
+#' Doesn't do anything useful yet, but the idea is to be able to use a
+#' formula for a twoway table in long form, e.g.,
+#' twoway(response ~ row + col, data=mydata)
+#'
+#' @param formula A formula of the form \code{response ~ rowvar + colVAR}
+#' @param data The name of the data set
+#' @param subset An expression to subset the data
+#' @param na.action What to do with NAs?
+#' @param ... other arguments, passed down
+#' @importFrom stats terms
+#'
 twoway.formula <- function(formula, data, subset, na.action, ...) {
 
   if (missing(formula) || !inherits(formula, "formula"))
@@ -6,18 +19,20 @@ twoway.formula <- function(formula, data, subset, na.action, ...) {
     stop("'formula' must have both left and right hand sides")
   tt <- if (is.data.frame(data))
     terms(formula, data = data)
-  else terms(formula, allowDotAsName = TRUE)
+  else terms(formula)
   if (any(attr(tt, "order") > 1))
     stop("interactions are not allowed")
 
   rvar <- attr(terms(formula[-2L]), "term.labels")
   cvar <- attr(terms(formula[-3L]), "term.labels")
-  rhs.has.dot <- any(rvars == ".")
-  lhs.has.dot <- any(cvars == ".")
-  if (lhs.has.dot && rhs.has.dot)
-    stop("'formula' has '.' in both left and right hand sides")
+  # rhs.has.dot <- any(rvars == ".")
+  # lhs.has.dot <- any(cvars == ".")
+  # if (lhs.has.dot && rhs.has.dot)
+  #   stop("'formula' has '.' in both left and right hand sides")
   m <- match.call(expand.dots = FALSE)
   edata <- eval(m$data, parent.frame())
+
+  stop("The formula method is not yet implemented.")
 
   # cl <- match.call()
   # mf <- match.call(expand.dots = FALSE)
