248c3c1b1b85f8536d6fe4d49c9534727c7110a5
Michael Friendly <friendly@yorku.ca>
2018-05-30 11:02:55 -0400

    `print.twoway()` now displays the `names(dimnames(x))` and the `responseName` when available [RMH].

--- R/print.twoway.R
+++ R/print.twoway.R
@@ -24,16 +24,18 @@
 print.twoway <-
   function (x, digits = getOption("digits"), border=2, zapsmall=TRUE, ...)
   {
-    title <- if(x$method == "mean")
-      "Mean decomposition "
-    else
-      "Median polish decomposition "
-    cat("\n", title, "(Dataset: \"", x$name, "\")\n", sep="")
+    title <- switch(x$method,
+                    mean="Mean decomposition ",
+                    median="Median polish decomposition ",
+                    "Initial data ")
+    cat("\n", title, '(Dataset: "', x$name, '"; Response: ', x$responseName, ')\n', sep="")
 
     if (border < 1) {
-      cat("\nOverall: ", x$overall, "\n\nRow Effects:\n", sep = "")
+      cat("\nOverall: ", x$overall, sep="")
+      cat("\n\nRow Effects: ", x$varNames[1], "\n", sep = "")
       print(x$roweff, digits = digits, ...)
       cat("\nColumn Effects:\n")
+      cat("\nColumn Effects: ", x$varNames[2], "\n", sep = "")
       print(x$coleff, digits = digits, ...)
       cat("\nResiduals:\n")
       resids <- if (zapsmall) zapsmall(x$residuals) else x$residuals
@@ -46,6 +48,7 @@ print.twoway <-
         cbind( resids, roweff=x$roweff ),
         coleff=    c( x$coleff, x$overall)
       )
+      names(dimnames(tbl)) <- names(dimnames(resids))
       print(tbl, digits = max(2L, digits - 2L), ...)
     }
     else {
@@ -64,6 +67,7 @@ print.twoway <-
       ccolon <- c("+", rep(":", nrow(tbl) - 1), "+", ":")
       tblc <- cbind(` ` = vvertical, tblr[, -ncol(tblr)], ` ` = ccolon,
                     roweff = tblr[, ncol(tblr)])
+      names(dimnames(tblc)) <- names(dimnames(x$residuals))
       print(tblc, quote = FALSE, ...)
     }
     cat("\n")
@@ -71,3 +75,54 @@ print.twoway <-
     invisible(x)
 
   }
+
+# print.twoway <-
+#   function (x, digits = getOption("digits"), border=2, zapsmall=TRUE, ...)
+#   {
+#     title <- if(x$method == "mean")
+#       "Mean decomposition "
+#     else
+#       "Median polish decomposition "
+#     cat("\n", title, "(Dataset: \"", x$name, "\")\n", sep="")
+#
+#     if (border < 1) {
+#       cat("\nOverall: ", x$overall, "\n\nRow Effects:\n", sep = "")
+#       print(x$roweff, digits = digits, ...)
+#       cat("\nColumn Effects:\n")
+#       print(x$coleff, digits = digits, ...)
+#       cat("\nResiduals:\n")
+#       resids <- if (zapsmall) zapsmall(x$residuals) else x$residuals
+#       print(resids, digits = max(2L, digits - 2L), ...)
+#     }
+#     else if (border == 1){
+#       cat("Residuals bordered by row effects, column effects, and overall\n\n")
+#       resids <- if (zapsmall) zapsmall(x$residuals) else x$residuals
+#       tbl <- rbind(
+#         cbind( resids, roweff=x$roweff ),
+#         coleff=    c( x$coleff, x$overall)
+#       )
+#       print(tbl, digits = max(2L, digits - 2L), ...)
+#     }
+#     else {
+#       cat("Residuals bordered by row effects, column effects, and overall\n\n")
+#       tbl <- rbind(cbind(x$residuals, roweff = x$roweff),
+#                    coleff = c(x$coleff,
+#                               x$overall))
+#       tblz <- if (zapsmall) zapsmall(tbl) else tbl
+#       tblf <- format(tblz, digits = max(2L, digits - 2L))
+#       nc <- nchar(tblf[1])
+#       hhyphen <- paste0(rep("-", nc), collapse = "")
+#       ddot <- paste0(rep(".", nc), collapse = "")
+#       tblr <- rbind(` ` = hhyphen, tblf[-nrow(tblf), ], ` ` = ddot,
+#                     coleff = tblf[nrow(tblf), ])
+#       vvertical <- c("+", rep("|", nrow(tbl) - 1), "+", "|")
+#       ccolon <- c("+", rep(":", nrow(tbl) - 1), "+", ":")
+#       tblc <- cbind(` ` = vvertical, tblr[, -ncol(tblr)], ` ` = ccolon,
+#                     roweff = tblr[, ncol(tblr)])
+#       print(tblc, quote = FALSE, ...)
+#     }
+#     cat("\n")
+#
+#     invisible(x)
+#
+#   }
