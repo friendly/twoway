9876e576a70a8b5e603a5067e9f727864d0d4a2d
Michael Friendly <friendly@yorku.ca>
2018-06-01 09:04:40 -0400

    add examples to plot.twoway.Rd

--- R/plot.twoway.R
+++ R/plot.twoway.R
@@ -21,6 +21,30 @@
 #' @importFrom stats lm coef
 #' @rdname plot.twoway
 #' @export
+#' @examples
+#' data(taskRT)
+#' tw <- twoway(taskRT)
+#' tw
+#' twmed <- twoway(taskRT, method="median")
+#' twmed
+
+#' plot(tw,    xlim=c(2,7), ylim=c(2,7)) ## use the same xlim and ylim, for comparison
+#' plot(twmed, xlim=c(2,7), ylim=c(2,7))
+#'
+#' plot(tw,    which="diagnose", xlim=c(-.19, .19), ylim=c(-.5, .55))
+#' plot(twmed, which="diagnose", xlim=c(-.19, .19), ylim=c(-.5, .55))
+#'
+#' data(insectCounts)
+#' twi <- twoway(insectCounts)
+#' twimed <- twoway(insectCounts, method="median")
+#'
+#' plot(twi,    xlim=c(-250, 700), ylim=c(-180, 900))
+#' plot(twimed, xlim=c(-250, 700), ylim=c(-180, 900))
+#'
+#' plot(twi,    which="diagnose", xlim=c(-160, 170), ylim=c(-200, 400))  ## power = .1
+#' plot(twimed, which="diagnose", xlim=c(-160, 170), ylim=c(-200, 400))  ## power = .3
+
+
 
 plot.twoway <- function(x,
                         which=c("fit", "diagnose"),
@@ -111,7 +135,7 @@ plot.twoway.fit <-
 ## diagnostic plot
 
 #' @details
-#'      If you want to use the diagnostic plot to compare the result of \code{method="mean"} and \code{method="median"}, it is
+#'      For both plots, if you want to directly compare the result of \code{method="mean"} and \code{method="median"}, it is
 #'      essential to set the same \code{xlim} and \code{ylim} axes in the call.
 #'
 #' @param annotate  A logical value; if \code{TRUE}, the slope and power are displayed in the diagnostic plot
