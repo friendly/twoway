e2c16519357f41aad5400755a7179d58ca7f90be
Michael Friendly <friendly@yorku.ca>
2018-08-24 22:03:01 -0400

    fix references

--- R/twoway.R
+++ R/twoway.R
@@ -30,6 +30,7 @@
 #' @author Michael Friendly
 #' @seealso code{\link{twoway.formula}}, code{\link[stats]{medpolish}}
 #' @references Tukey, J. W. (1977). \emph{Exploratory Data Analysis}, Reading MA: Addison-Wesley.
+#'
 #'             Friendly, M. (1991). \emph{SAS System for Statistical Graphics} Cary, NC: SAS Institute
 #' @export
 
